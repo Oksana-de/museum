9cf8717d882b5f404aaabd3e47fd1c52c3577630
Oksana Vovere <oksana.vovere@gmail.com>
2022-11-22 12:27:27 +0100

    feat: implement the opening-closing of the ticket form

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import "./styles/sass/main.scss";
-import "./ts/burger-menu.ts";
-import "./ts/gallery.ts";
+import './styles/sass/main.scss';
+import './ts/burger-menu.ts';
+import './ts/gallery.ts';
+import './ts/ticket-form.ts'
